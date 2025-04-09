fc77b5d3555c22d9ef6e0fd92132296d20689f15
itsjoacor <jdr123r@gmail.com>
2025-04-09 20:44:51 -0300

    wip: jump n gravity

--- entrega-3-rodriguez-joaquin/fullblackboxv3/Player/player.gd
+++ entrega-3-rodriguez-joaquin/fullblackboxv3/Player/player.gd
@@ -1,5 +1,19 @@
 extends CharacterBody2D
 
-func _physics_process(_delta):
-	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 150
+var speed := 120
+var direccion := 0.0
+var jump := 250
+const gravity := 9
+var jump_times := 0
+
+func _physics_process(delta):
+	direccion = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direccion * speed
+
+	if (jump_times < 3) and Input.is_action_just_pressed("ui_accept"):
+		velocity.y -= jump
+		jump_times += 1
+	if is_on_floor():
+		jump_times = 0
+	velocity.y += gravity
 	move_and_slide()
