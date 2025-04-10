a04aafd7fad0a5000e7ccbc2042fcd1ebb15150f
itsjoacor <jdr123r@gmail.com>
2025-04-09 23:33:54 -0300

    finished 1st mvp - retocar

--- entrega-3-rodriguez-joaquin/fullblackboxv3/Turret/TurretBullet/turret_bullet.gd
+++ entrega-3-rodriguez-joaquin/fullblackboxv3/Turret/TurretBullet/turret_bullet.gd
@@ -3,8 +3,16 @@ extends Area2D
 var direction: Vector2 = Vector2.RIGHT
 var speed: float = 300
 
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
 func _physics_process(delta):
 	position += direction * speed * delta
 
 func _on_screen_exited():
 	queue_free()
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://GameOver/GameOver.tscn")
+		queue_free()
